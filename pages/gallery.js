9547d3c072f3efcc6cc823a2636f31ee28193610
KrKamn <ales.kamnikar@gmail.com>
2026-01-28 22:19:12 +0100

    dodane 16 slik

--- pages/gallery.js
+++ pages/gallery.js
@@ -7,7 +7,24 @@ export default function GalleryPage() {
   const { locale } = useRouter();
   const t = translations[locale] || translations.si;
 
-  const images = ["/gallery/01.jpg", "/gallery/02.jpg", "/gallery/03.jpg", "/gallery/04.jpg"];
+  const images = [
+  "/gallery/01.jpg",
+  "/gallery/02.jpg",
+  "/gallery/03.jpg",
+  "/gallery/04.jpg",
+  "/gallery/05.jpg",
+  "/gallery/06.jpg",
+  "/gallery/07.jpg",
+  "/gallery/08.jpg",
+  "/gallery/09.jpg",
+  "/gallery/10.jpg",
+  "/gallery/11.jpg",
+  "/gallery/12.jpg",
+  "/gallery/13.jpg",
+  "/gallery/14.jpg",
+  "/gallery/15.jpg",
+  "/gallery/16.jpg",
+];
 
   return (
     <Layout>
